5de75207e772e163bcadcf7850bdbe05c7ccefb2
Moritz Schmale <narrow.m@gmail.com>
2017-10-18 13:59:11 +0200

    added ecto types

---
+++ lib/repo/action_type.ex
@@ -0,0 +1,29 @@
+defmodule ExAudit.Type.Action do
+  @behaviour Ecto.Type
+
+  @actions ~w[created updated deleted]a
+
+  for action <- @actions do
+    def cast(unquote(Atom.to_string(action))) do
+      {:ok, unquote(action)}
+    end
+
+    def cast(unquote(action)) do
+      {:ok, unquote(action)}
+    end
+
+    def load(unquote(Atom.to_string(action))) do
+      {:ok, unquote(action)}
+    end
+
+    def dump(unquote(action)) do
+      {:ok, unquote(Atom.to_string(action))}
+    end
+  end
+
+  def dump(_), do: :error
+  def load(_), do: :error
+  def cast(_), do: :error
+
+  def type, do: :string
+end
---
+++ lib/repo/patch_type.ex
@@ -0,0 +1,8 @@
+defmodule ExAudit.Type.Patch do
+  @behaviour Ecto.Type
+
+  def cast(a), do: {:ok, a}
+  def dump(patch), do: {:ok, :erlang.term_to_binary(patch)}
+  def load(binary), do: {:ok, :erlang.binary_to_term(binary)}
+  def type, do: :binary
+end
