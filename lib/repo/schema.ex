d7cd387a8f33259e3191cd2d1e56b36f0689eb24
Moritz Schmale <narrow.m@gmail.com>
2017-10-19 17:16:01 +0200

    use transactions to add version entries

--- lib/repo/schema.ex
+++ lib/repo/schema.ex
@@ -7,30 +7,34 @@ defmodule ExAudit.Schema do
 
   def insert(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.insert(module, adapter, struct, opts)
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.insert(module, adapter, struct, opts)
 
-    case result do
-      {:ok, resulting_struct} ->
-        ExAudit.Tracking.track_change(module, adapter, :created, struct, resulting_struct, opts)
-      _ -> 
-        :ok
-    end
+      case result do
+        {:ok, resulting_struct} ->
+          ExAudit.Tracking.track_change(module, adapter, :created, struct, resulting_struct, opts)
+        _ -> 
+          :ok
+      end
 
-    result
+      result
+    end)
   end
 
   def update(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.update(module, adapter, struct, opts)
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.update(module, adapter, struct, opts)
 
-    case result do
-      {:ok, resulting_struct} ->
-        ExAudit.Tracking.track_change(module, adapter, :updated, struct, resulting_struct, opts)
-      _ -> 
-        :ok
-    end
+      case result do
+        {:ok, resulting_struct} ->
+          ExAudit.Tracking.track_change(module, adapter, :updated, struct, resulting_struct, opts)
+        _ -> 
+          :ok
+      end
 
-    result
+      result
+    end)
   end
 
   def insert_or_update(module, adapter, changeset, opts) do
@@ -41,30 +45,36 @@ defmodule ExAudit.Schema do
 
   def delete(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.delete(module, adapter, struct, opts)
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.delete(module, adapter, struct, opts)
 
-    case result do
-      {:ok, resulting_struct} ->
-        ExAudit.Tracking.track_change(module, adapter, :deleted, struct, resulting_struct, opts)
-      _ -> 
-        :ok
-    end
+      case result do
+        {:ok, resulting_struct} ->
+          ExAudit.Tracking.track_change(module, adapter, :deleted, struct, resulting_struct, opts)
+        _ -> 
+          :ok
+      end
 
-    result
+      result
+    end)
   end
 
   def insert!(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.insert!(module, adapter, struct, opts)
-    ExAudit.Tracking.track_change(module, adapter, :created, struct, result, opts)
-    result
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.insert!(module, adapter, struct, opts)
+      ExAudit.Tracking.track_change(module, adapter, :created, struct, result, opts)
+      result
+    end)
   end
 
   def update!(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.update!(module, adapter, struct, opts)
-    ExAudit.Tracking.track_change(module, adapter, :updated, struct, result, opts)
-    result
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.update!(module, adapter, struct, opts)
+      ExAudit.Tracking.track_change(module, adapter, :updated, struct, result, opts)
+      result
+    end)
   end
 
   def insert_or_update!(module, adapter, changeset, opts) do
@@ -75,9 +85,22 @@ defmodule ExAudit.Schema do
 
   def delete!(module, adapter, struct, opts) do
     opts = augment_opts(opts)
-    result = Ecto.Repo.Schema.delete!(module, adapter, struct, opts)
-    ExAudit.Tracking.track_change(module, adapter, :deleted, struct, result, opts)
-    result
+    augment_transaction(module, fn ->
+      result = Ecto.Repo.Schema.delete!(module, adapter, struct, opts)
+      ExAudit.Tracking.track_change(module, adapter, :deleted, struct, result, opts)
+      result
+    end)
+  end
+
+  defp augment_transaction(repo, fun) do
+    case repo.in_transaction?() do
+      true -> fun.()
+      false ->
+        case repo.transaction(fun) do
+          {:ok, value} -> value
+          other -> other
+        end
+    end
   end
 
   defp augment_opts(opts) do
