287e31b4719f4e51c11a7aba346088d88a340392
Roman Heinrich <roman.heinrich@gmail.com>
2020-01-29 22:38:42 +0100

    Automatically run migration for unit tests
    Problem: 
    - currently migrations have to be executed prior to running tests
    - that is not very user-friendly 
    
    Solution: 
    - Execute them in test_helper.ex

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,8 @@
 ExAudit.Test.Repo.start_link()
 Ecto.Adapters.SQL.Sandbox.mode(ExAudit.Test.Repo, :auto)
 
+
+migrations_path = Path.join([:code.priv_dir(:ex_audit), "repo", "migrations"])
+Ecto.Migrator.run(ExAudit.Test.Repo, migrations_path, :up, all: true)
+
 ExUnit.start()
