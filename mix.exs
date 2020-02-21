e71c78e7df3d3b9920ff65830794811ac6b2d9e0
Gustavo Chapim <gustavo.chapim@petlove.com.br>
2020-02-20 21:02:13 -0300

    Change hex version strategy

--- mix.exs
+++ mix.exs
@@ -49,9 +49,9 @@ defmodule ExAudit.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.2.0"},
-      {:ecto_sql, "~> 3.2.0"},
-      {:postgrex, "~> 0.15.0", only: :test},
+      {:ecto, ">= 3.2.0"},
+      {:ecto_sql, ">= 3.2.0"},
+      {:postgrex, ">= 0.15.0", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
       {:ex_doc, "~> 0.21", runtime: false, only: :dev}
     ]
