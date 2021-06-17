122fc797595bae7f9e9e27b213970ce6a4fba254
Moritz Schmale <narrow.m@gmail.com>
2021-06-17 11:46:20 +0200

    v0.8

--- mix.exs
+++ mix.exs
@@ -5,8 +5,8 @@ defmodule ExAudit.Mixfile do
     [
       description: "Ecto auditing library that transparently tracks changes and can revert them",
       app: :ex_audit,
-      version: "0.7.0",
-      elixir: "~> 1.6",
+      version: "0.8.0",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: paths(Mix.env()),
