8ab7c4653775afc5345bb00c7d422e37610a378b
Roberts Gulans <rgulans@gmail.com>
2021-12-03 12:43:26 +0200

    fixed warning from: https://github.com/ZennerIoT/ex_audit/pull/78

--- lib/repo/repo.ex
+++ lib/repo/repo.ex
@@ -197,10 +197,6 @@ defmodule ExAudit.Repo do
         end
       end
 
-      def default_options(_operation), do: []
-
-      defoverridable(default_options: 1)
-
       defoverridable(child_spec: 1)
 
       # additional functions
@@ -237,6 +233,4 @@ defmodule ExAudit.Repo do
   """
   @callback revert(version :: struct, opts :: list) ::
               {:ok, struct} | {:error, changeset :: Ecto.Changeset.t()}
-
-  @callback default_options(operation :: atom) :: keyword
 end
