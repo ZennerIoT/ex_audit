6ab62813bb76aa39876404bd71ddc3707d0a16e8
Aaron Tavistock <aaron@geekshow.com>
2022-09-08 00:00:27 -0700

    Change storage to jsonb

--- lib/types/patch_type.ex
+++
@@ -1,8 +0,0 @@
-defmodule ExAudit.Type.Patch do
-  use Ecto.Type
-
-  def cast(a), do: {:ok, a}
-  def dump(patch), do: {:ok, :erlang.term_to_binary(patch)}
-  def load(binary), do: {:ok, :erlang.binary_to_term(binary)}
-  def type, do: :binary
-end
